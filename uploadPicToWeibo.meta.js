fcaa27ef4e5bfdabdb1cbbaf928ded55ec1e0ba5
holynewbie <holynewbie@users.noreply.github.com>
2016-04-29 22:20:37 +0800

    Create uploadPicToWeibo.meta.js
    新增脚本的更新链接

---
+++ uploadPicToWeibo.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        uploadPicToWeibo
+// @description upload your local picture to sina weibo
+// @version     0.0.3
+// @namespace   happycheng10@126.com
+// @include     http://photo.weibo.com/upload/index
+// @updateURL   https://holynewbie.github.io/uploadPicToWeibo/uploadPicToWeibo.meta.js
+// @downloadURL https://holynewbie.github.io/uploadPicToWeibo/uploadPicToWeibo.user.js
+// @grant       GM_setClipboard
+// ==/UserScript==
