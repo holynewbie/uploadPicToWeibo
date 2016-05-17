5fd31929077c55d789c5f58a84d9c14a2282b74a
holynewbie <holynewbie@users.noreply.github.com>
2016-05-17 13:33:11 +0800

    Update BinaryPost.js
    修复了一些错误

--- BinaryPost.js
+++ BinaryPost.js
@@ -15,27 +15,24 @@ var BinaryPost = BinaryPost || (function(GM_POST){
 	return function (obj) {
 		var body = [];
 		var boundary = getBoundary();
-		var res = {
-			abort:function(){}
-		};
 		Promise.all((Object.getOwnPropertyNames(obj.data)).map(function(filedName){
-			if(obj.data[filedName] instanceof Blob)
-			return new Promise(function(success){
-				let fr = new FileReader();
-				fr.onload = function(){
-					body.push('--' + boundary);
-					var disp = 'Content-Disposition: form-data; name="' + filedName + '"';
-					if(obj.data[filedName].name){
-						disp += '; filename="' + obj.data[filedName].name + '"';
-					}
-					body.push(disp);
-					body.push('Content-type: ' + obj.data[filedName].type);
-					body.push('');
-					body.push(fr.result);
-					success();
-				};
-				fr.readAsBinaryString(obj.data[filedName]);
-			});
+			if(obj.data[filedName] instanceof Blob){
+				return new Promise(function(success){
+					let fr = new FileReader();
+					fr.onload = function(){
+						body.push('--' + boundary);
+						var disp = 'Content-Disposition: form-data; name="' + filedName + '"';
+						//sometimes it must has a filename
+						disp += '; filename="' + (obj.data[filedName].name||'BinaryPost') + '"';
+						body.push(disp);
+						body.push('Content-type: ' + obj.data[filedName].type);
+						body.push('');
+						body.push(fr.result);
+						success();
+					};
+					fr.readAsBinaryString(obj.data[filedName]);
+				});
+			}
 			body.push('Content-Disposition: form-data; name="' + filedName + '"');
 			body.push('');
 			body.push(obj.data[filedName]);
@@ -47,6 +44,7 @@ var BinaryPost = BinaryPost || (function(GM_POST){
 			if(obj.headers){ headers = obj.headers; }
 			headers["Content-Type"] = 'multipart/form-data; boundary=' + boundary;
 			headers["Content-Length"] = data.length
+			console.log('数据总长度:'+data.length);
 			var request = {
 				method: 'POST',
 				url: obj.url,
@@ -56,27 +54,9 @@ var BinaryPost = BinaryPost || (function(GM_POST){
 				onload: obj.onload,
 				ontimeout: obj.onload,
 				timeout: obj.timeout || 2000,
+				onerror:obj.onload,
 			};
 			GM_POST(request);
 		});
 	}
 })(GM_xmlhttpRequest);
-
-function getBase64Image(img) {
-    var canvas = document.createElement("canvas");
-    canvas.width = img.naturalWidth ;
-    canvas.height = img.naturalHeight;
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-    var dataURL = canvas.toDataURL("image/jpg");
-    return dataURL;//.replace(/^data:image\/(png|jpg);base64,/, "");
-}
-function dataURItoBlob(dataURI) {
-    var byteString = atob(dataURI.split(',')[1]);
-    var ab = new ArrayBuffer(byteString.length);
-    var ia = new Uint8Array(ab);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-    return new Blob([ab], { type: 'image/jpeg' });
-}
