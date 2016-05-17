7401568031fc06a38d3fc7af00572b12fde92e34
holynewbie <holynewbie@users.noreply.github.com>
2016-05-17 13:39:06 +0800

    Create uploadPicFromDoubanToWeibo.meta.js

---
+++ uploadPicFromDoubanToWeibo.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        uploadPicFromDoubanToWeibo
+// @description upload a douban poster to sina weibo
+// @version     0.0.1
+// @namespace   happycheng10@126.com
+// @include     *.doubanio.com/view/photo/raw/public/*
+// @require     https://raw.githubusercontent.com/holynewbie/uploadPicToWeibo/gh-pages/BinaryPost.js
+// @updateURL   https://holynewbie.github.io/uploadPicToWeibo/uploadPicFromDoubanToWeibo.meta.js
+// @downloadURL https://holynewbie.github.io/uploadPicToWeibo/uploadPicFromDoubanToWeibo.user.js
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
