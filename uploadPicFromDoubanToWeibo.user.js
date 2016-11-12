216d2276c533a689b38c317662bedaffe037b4ee
程成 <holynewbie@users.noreply.github.com>
2016-11-12 23:06:10 +0800

    Update uploadPicFromDoubanToWeibo.user.js

--- uploadPicFromDoubanToWeibo.user.js
+++ uploadPicFromDoubanToWeibo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        uploadPicFromDoubanToWeibo
 // @description upload a douban poster to sina weibo
-// @version     0.0.
+// @version     0.0.2
 // @namespace   happycheng10@126.com
 // @include     *.doubanio.com/view/photo/raw/public/*
 // @require     https://raw.githubusercontent.com/holynewbie/uploadPicToWeibo/gh-pages/BinaryPost.js
