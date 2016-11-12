e3ccd40a2ea9f7522b5f390a2da89aafeb584ea2
程成 <holynewbie@users.noreply.github.com>
2016-11-12 23:06:42 +0800

    Update uploadPicToWeibo.meta.js

--- uploadPicToWeibo.meta.js
+++ uploadPicToWeibo.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        uploadPicToWeibo
 // @description upload your local picture to sina weibo
-// @version     0.0.3
+// @version     0.0.4
 // @namespace   happycheng10@126.com
 // @include     http://photo.weibo.com/upload/index
 // @updateURL   https://holynewbie.github.io/uploadPicToWeibo/uploadPicToWeibo.meta.js
